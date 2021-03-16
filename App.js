7d6975147398c9f99c6fa3aed3a4612918ecaeaa
Tobi <tobi@tobis-mbp.attlocal.net>
2021-03-16 12:51:40 -0500

    adding main pages, bottom navigation, and stack navigation

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Dashboard, Place } from "./screens";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+import BottomTabNav from "./navigation/BottomTabNav";
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName={"MainApp"}
+      >
+        <Stack.Screen name="MainApp" component={BottomTabNav} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ screens/Maintenance.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Maintenance = () => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Maintenance Page</Text>
+    </View>
+  );
+};
+
+export default Maintenance;
