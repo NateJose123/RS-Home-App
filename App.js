411fa513257d9ed9a054965599d5943e6012b4d4
NateJose123 <josen@berea.edu>
2021-05-04 17:37:20 -0500

    setup base off meals to go

--- App.js
+++ App.js
@@ -1,25 +1,53 @@
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
-import { Dashboard, Place } from "./screens";
-import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
+import { ThemeProvider } from "styled-components/native";
+import * as firebase from "firebase";
 
-import BottomTabNav from "./navigation/BottomTabNav";
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
-const Stack = createStackNavigator();
+import { theme } from "./src/infrastructure/theme";
+import { Navigation } from "./src/infrastructure/navigation";
 
-const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-        initialRouteName={"Testing"}
-      >
-        <Stack.Screen name="MainApp" component={BottomTabNav} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBJvtuHuSbYLg4mBGGx7CjJhrqDonwZxNc",
+  authDomain: "mealstogo-df9bb.firebaseapp.com",
+  projectId: "mealstogo-df9bb",
+  storageBucket: "mealstogo-df9bb.appspot.com",
+  messagingSenderId: "116121312870",
+  appId: "1:116121312870:web:48170fccd2a1f6ef9a97a7",
 };
 
-export default App;
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <AuthenticationContextProvider>
+          <Navigation />
+        </AuthenticationContextProvider>
+      </ThemeProvider>
+      <ExpoStatusBar style="auto" />
+    </>
+  );
+}
